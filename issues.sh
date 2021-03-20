98e77545948bb18db1c1b048130cd6b5b2c622b8
David Härer <haerer@locandis.de>
2021-03-20 23:37:48 +0100

    Use additional tmp file.

--- issues.sh
+++ issues.sh
@@ -8,8 +8,11 @@ set -Eeuo pipefail
 LABELS='TODO|FIXME|BUG|NOTE|XXX|HACK|FEATURE|IDEA|WISH'
 HEADING="Issues"
 OUTFILE="ISSUES.md"
-TMPFILE=".issues.tmp"
-TMPIGNOREFILE=".issuesignore.tmp"
+
+# Define names of tmp files. 
+TMP_ISSUES=".issues.tmp"
+TMP_IGNORE=".issuesignore.tmp"
+TMP_FILES=".issuefiles.tmp"
 
 # Get the root path of the repository.
 ROOT=$(git rev-parse --show-toplevel)
@@ -27,9 +30,17 @@ REPLACE_PATTERN='- [ ] [\3#L\2](\1#L\2): \4'
 if [[ -f "$ROOT/.issuesignore" ]]
 then
     # Remove comments from the .issueignore file and save it in a tmp file. 
-    grep "^[^#]" "$ROOT/.issuesignore" > "$ROOT/$TMPIGNOREFILE"
+    git grep "^[^#]" HEAD -- "$ROOT/.issuesignore" \
+        | sed 's/^HEAD:.issuesignore://' \
+        > "$ROOT/$TMP_IGNORE" \
+        || true
+
+    git ls-files \
+        | grep -v -E -f "$ROOT/$TMP_IGNORE" \
+        > "$ROOT/$TMP_FILES" \
+        || true
 else
-    echo "" > "$ROOT/$TMPIGNOREFILE"
+    git ls-files > "$ROOT/$TMP_FILES"
 fi
 
 # Write the heading.
@@ -37,13 +48,13 @@ echo '# '"$HEADING"
 echo ""
 
 # Loop over all files tracked in the repository that are not ignored.
-for file in $(git ls-files | grep -v -E -f "$ROOT/$TMPIGNOREFILE")
+for file in $(cat "$ROOT/$TMP_FILES")
 do
     # Extract all lines of the file at HEAD, that have one of the defined issue labels.
     git grep -n -E '('"$LABELS"'): ' "HEAD" -- "$ROOT/$file" > "$ROOT/$TMP_ISSUES" || true
 
     # Continue with the next file, if no lines where extracted.
-    if [[ ! -s "$ROOT/$TMPFILE" ]]
+    if [[ ! -s "$ROOT/$TMP_ISSUES" ]]
     then
         continue
     fi
@@ -53,14 +64,14 @@ do
     echo ""
 
     # Transform and write the raw lines in Markdown.
-    cat "$ROOT/$TMPFILE" | sed 's/'"$SOURCE_PATTERN"'/'"$REPLACE_PATTERN"'/'
+    cat "$ROOT/$TMP_ISSUES" | sed 's/'"$SOURCE_PATTERN"'/'"$REPLACE_PATTERN"'/'
     
     # Write a newline as a spacer for the next section.
     echo ""
 done
 
 # Clean-up the tmp files.
-rm -f "$ROOT/$TMPFILE" "$ROOT/$TMPIGNOREFILE"
+rm -f "$ROOT/$TMP_ISSUES" "$ROOT/$TMP_IGNORE" "$ROOT/$TMP_FILES"
 
 # Commit the update.
 git add $OUTFILE
